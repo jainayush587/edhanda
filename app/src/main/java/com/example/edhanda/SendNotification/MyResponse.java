d854e09bf741dd496664dbc2e9076f7ee61e7b65
Ayush Jain <jainayush587@gmail.com>
2020-12-19 19:45:38 +0530

    Beta version commit

---
+++ app/src/main/java/com/example/edhanda/SendNotification/MyResponse.java
@@ -0,0 +1,6 @@
+package com.example.edhanda.SendNotification;
+
+public class MyResponse {
+
+    public int success;
+}
---
+++ app/src/main/java/com/example/edhanda/User.java
@@ -0,0 +1,11 @@
+package com.example.edhanda;
+
+public class User  {
+
+    String Role;
+
+    public User(String role)
+    {
+        Role=role;
+    }
+}
